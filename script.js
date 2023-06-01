a499b55c678b3c2de5be3cd080693f541e034900
topoti <ts03351@gmail.com>
2023-06-01 15:02:05 +0600

    commit

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 const inputField = document.getElementById('inputField')
 const addButton = document.getElementById('addButton')
 const listItem = document.getElementById('list')
+const allButton = document.getElementById('allButton')
 
 function clickButton() {
     addTodo()
 }
+
+inputField.addEventListener("keydown", function (event) {
+    if (event.key === "Enter"){
+        addTodo();
+    }
+});
+
+allButton.addEventListener('click', addTodo);
 
 function addTodo() {
     const itemall = document.createElement('div')
@@ -21,18 +30,53 @@ function addTodo() {
     listItem.appendChild(itemall)
     inputField.value = ''
 
+  
+    const checkbutton = document.createElement("button")
+    checkbutton.classList.add("fas", "fa-check");
+    itemall.appendChild(checkbutton)
+
+
     const trashbutton = document.createElement("button")
     trashbutton.classList.add("fas", "fa-trash");
     itemall.appendChild(trashbutton)
+
 }
 
-function del(){
-    if(item.classList[0] =='trashbutton'){
-        const listItem = item.parentElement
-        listItem.remove()
+function clickHandler(e){
+    const item = e.target
+
+    if(item.classList.contains('fa-check')){
+        // check button is clicked
+        if (item.classList[0] === 'fas') {
+            const todolist = item.parentElement
+            todolist.classList.toggle('checklist')
+        }
+    
+    } else if (item.classList.contains('fa-trash')){
+        // delete button is clicked
+        if (item.classList[0] === 'fas') {
+        const todolist = item.parentElement
+        todolist.remove()
+    }  
+
     }
 }
 
-listItem.addEventListener('click', del);
+listItem.addEventListener('click', clickHandler);
 
 addButton.addEventListener('click', clickButton);
+
+for (let i = 0; i < updatedList.length; i++) {
+    if (updatedList[i].id == listItem.id) {
+      updatedList.splice(updatedList.indexOf(updatedList[i]), 1);
+    }
+    console.log(updatedList);
+  }
+
+
+  for( let i=0; i< itemall.length; i++){
+    if(itemall[i].id == listItem.id){
+        itemall[i].splice(itemall.indexOf(itemall[i]),1);
+    }
+    console.log(itemall);
+  }
