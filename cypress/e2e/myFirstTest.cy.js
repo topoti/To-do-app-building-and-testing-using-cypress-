17acfd7d0bc3fa44c239cd0a114ff47515ef024c
topoti <ts03351@gmail.com>
2023-06-02 18:35:10 +0600

    commit

--- cypress/e2e/myFirstTest.cy.js
+++ cypress/e2e/myFirstTest.cy.js
@@ -2,11 +2,16 @@ describe("My First Test", () => {
  it("test1", () =>{
 
     //test case
-    // cy.get("textarea")
-    // .type("my first test");
-    
+    cy.visit("http://127.0.0.1:5501/index.html");
     cy.get("h1")
     .should("contain", "To Do List");
 
+    cy.get(".textarea") 
+    .type("my first test");
+
+    cy.contains("Add Item").click();
+
  })
+
+
 })
